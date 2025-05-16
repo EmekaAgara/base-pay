a133aea9dfc733f43f0009332a02ad9c7e9ee375
EmekaAgara <emekaagara@gmail.com>
2025-05-16 23:38:35 +0100

    featured

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -141,10 +141,9 @@ const Product = () => {
         <div className="flex flex-col items-center">
           <div className="flex flex-col items-center mb-4 mt-16">
             <p className="text-3xl font-medium">
-              Featured{" "}
+              Recomended{" "}
               <span className="font-medium text-orange-600">Products</span>
             </p>
-            <div className="w-28 h-0.5 bg-orange-600 mt-2"></div>
           </div>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-6 pb-14 w-full">
             {products.slice(0, 5).map((product, index) => (
